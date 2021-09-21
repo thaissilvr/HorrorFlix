992a13de9c40f29d7ea105ce792d8a7940798b17
Renan Martins <82300584+renanmartins1813@users.noreply.github.com>
2021-09-21 16:04:53 -0500

    qualquer coisa favor chamar o matheus

--- js/app.js
+++ js/app.js
@@ -1,20 +1,32 @@
 const URLa = "http://www.omdbapi.com/?i=";
 const URLb = "&apikey=ced17615";
-
-const IDs = ["tt5052448", "tt9347730", "tt6857112", "tt1457767","tt3065204", "tt7069210" ,"tt6566576" ,"tt9701940", "tt9701942", "tt1396484", "tt7349950", "tt0053891"]
-
+const glide = document.getElementById("glide")
+const IDs = ["tt5052448", "tt9347730", "tt6857112", "tt0404032", "tt3567288",  "tt1457767","tt3065204", "tt7069210" ,"tt6566576" ,"tt9701940", "tt9701942", "tt1396484", "tt7349950", "tt0053891"]
+function img(img){
+    return `<li class="glide__slide" style="width: 310px; background-image: url(${img});background-size: cover; background-repeat: no-repeat; background-position: center;" ></li>`
+}
 IDs.forEach((id, i)=>{
     console.log(`ID[${i}] = ${id}`);
+    $.ajax({
+        url: `${URLa}${id}${URLb}`,
+        success: function(data){
+            console.log(data);
+            const imgURL = data.Poster;
+            console.log(imgURL);
+            glide.innerHTML += img(imgURL);
+        },
+        error: function(error){
+            console.log(error)
+        }
+    })
 })
 
-$.ajax({
-    url: `${URLa}${IDs[0]}${URLb}`,
-    success: function(data){
-        console.log(data)
-        return console.log(data);
-    },
-    error: function(error){
-        console.log(error)
 
-    }
-})
+// let slider=new Glider(document.querySelector('.glide'), {
+//     slidesToShow: 6,
+//     draggable: true,
+//     arrows: {
+//       prev: '.glider-prev',
+//       next: '.glider-next'
+//     }
+//   });
--- modal.js
+++ modal.js
@@ -1,6 +1,6 @@
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
+// var myModal = document.getElementById('myModal')
+// var myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
+// myModal.addEventListener('shown.bs.modal', function () {
+//   myInput.focus()
+// })
