2911465c34bc0b309cc62c5d325a8d88e5af3703
Renan Martins <82300584+renanmartins1813@users.noreply.github.com>
2021-09-21 23:03:47 -0500

    Revert "coisas mudaram 🙃"

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const URLa = "https://www.omdbapi.com/?i=";
+const URLa = "http://www.omdbapi.com/?i=";
 const URLb = "&apikey=ced17615";
 const glide = document.getElementById("glide")
 const IDs = ["tt5052448", "tt9347730", "tt6857112", "tt0404032", "tt3567288", "tt1591095", "tt1457767","tt3065204", "tt7069210" ,"tt6566576" ,"tt9701940", "tt9701942", "tt1396484", "tt7349950", "tt0053891"]
